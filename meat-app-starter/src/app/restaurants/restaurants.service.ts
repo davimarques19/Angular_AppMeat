31dde33c17eab4c307d1080815d09d261ca5c91d
Davi Marques <daviestudos19@gmail.com>
2020-03-05 10:12:49 -0300

    Adicionando tratamento de erro Catch

--- meat-app-starter/src/app/restaurants/restaurants.service.ts
+++ meat-app-starter/src/app/restaurants/restaurants.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core'
 import { Http } from '@angular/http'
 import { Observable } from 'rxjs/Observable'
 import { Restaurant } from './restaurant/restaurant.model'
-import 'rxjs/add/operator/map'
 import { MEAT_API } from '../app.api'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/operator/map'
+import { ErrorHandler } from '../app.error-handler'
 
 @Injectable()
 export class RestaurantsService {
   constructor(private http: Http) {}
 
   restaurants(): Observable<Restaurant[]> {
-    return this.http.get(`${MEAT_API}/restaurants`)
+    return this.http.get(`${MEAT_API}/restaurants1`)
       .map(response => response.json())
+      .catch(ErrorHandler.handleError)
   }
 }
