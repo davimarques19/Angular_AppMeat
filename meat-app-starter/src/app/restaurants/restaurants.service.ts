301ededb545aad0c5693b11c7f726dea1d0f93af
Davi Marques <daviestudos19@gmail.com>
2020-03-11 17:05:58 -0300

    Adicionando e removendo itens carrinho, add pt-BR

--- meat-app-starter/src/app/restaurants/restaurants.service.ts
+++ meat-app-starter/src/app/restaurants/restaurants.service.ts
@@ -6,6 +6,7 @@ import { MEAT_API } from '../app.api'
 import 'rxjs/add/operator/catch'
 import 'rxjs/add/operator/map'
 import { ErrorHandler } from '../app.error-handler'
+import { MenuItem } from 'app/restaurant-detail/menu-item/menu-item.model'
 
 @Injectable()
 export class RestaurantsService {
@@ -21,6 +22,18 @@ export class RestaurantsService {
     return this.http.get(`${MEAT_API}/restaurants/${id}`)
     .map(response => response.json())
     .catch(ErrorHandler.handleError)
+  }
+
+  reviewsOfRestaurant(id: string): Observable<any>{
+    return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
+  }
+
+  menuOfRestaurant(id: string): Observable<MenuItem[]>{
+    return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
   }
 
 }
